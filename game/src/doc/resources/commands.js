373635ebc0d33339a179431cdea75237dc404963
Michael Plüss <michael@pluess.net>
2020-11-05 22:11:59 +0100

    Made the command reference manual pretty

--- game/src/doc/resources/commands.js
+++ game/src/doc/resources/commands.js
@@ -47,7 +47,7 @@ const commands = [
             {
                 name: 'attributes',
                 type: 'List',
-                description: `The list of attributes that should be attached to the effect`,
+                description: `The list of attributes that should be attached to the effect`
             }
         ],
         description:   `Adds an effect to the given instance and returns the effect.<br>
@@ -56,6 +56,28 @@ const commands = [
                         Additionally, you can define a list of attributes that should be added to the effect.`,
         example:       `addEffect(self, "Well Rested", 12, ["attHealth", 5, "attStress", -10]);<br>
                         <i>// Adds an effect to self for 12 ticks that adds 5 to health and reduces stress by 10</i>`,
+    },
+    {
+        name: 'addPersonalAtt',
+        type: 'Number',
+        parameters: [
+            {
+                name: 'target',
+                type: 'Instance'
+            },
+            {
+                name: 'attribute',
+                type: 'String',
+                description: `The text ID of the attribute (eg. 'attHealth')`
+            },
+            {
+                name: 'amount',
+                type: 'Number'
+            }
+        ],
+        description:    `Adds the specified amount to the attribute value with the given text ID to the target instance.`,
+        example:        `addPersonalAtt(self, "attHealth", 7);<br>
+                         <i>// Adds 7 to the health attribute of self</i>`,
     },
     {
         name: 'random',
@@ -76,6 +98,6 @@ const commands = [
         ],
         description:   `Returns a random integer number that ranges from the lower bound (default 0) to the upper bound.<br>
                         More specifically, the range is [lower, upper), which means that the upper bound is never returned.<br>
-                        If only one parameter is given and it has the type List, then a random element from the list is returned.`
+                        If only one parameter is given and it has the type List, then a random element from the list is returned.`,
     },
 ];
