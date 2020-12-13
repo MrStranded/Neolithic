db6a1567d8c8ffe0ac94a24f9267ce44290a794e
Michael Plüss <michael@pluess.net>
2020-12-14 00:15:52 +0100

    Added return statement. Active drive is now displayed

---
+++ game/src/main/java/engine/data/interaction/SelectedInstance.java
@@ -0,0 +1,4 @@
+package engine.data.interaction;
+
+public class SelectedInstance {
+}
---
+++ game/src/main/java/engine/parser/scripts/nodes/ReturnStatementNode.java
@@ -0,0 +1,41 @@
+package engine.parser.scripts.nodes;
+
+import engine.data.entities.Instance;
+import engine.data.scripts.Script;
+import engine.data.variables.Variable;
+import engine.parser.scripts.exceptions.ScriptInterruptedException;
+
+public class IfStatementNode extends AbstractScriptNode {
+
+	public IfStatementNode(AbstractScriptNode condition, AbstractScriptNode body, AbstractScriptNode elseBody) {
+		subNodes = new AbstractScriptNode[3];
+		subNodes[0] = condition;
+		subNodes[1] = body;
+		subNodes[2] = elseBody;
+	}
+
+	@Override
+	public Variable execute(Instance instance, Script script) throws ScriptInterruptedException {
+		Variable condition = subNodes[0].execute(instance, script);
+
+		if (! condition.isNull()) {
+			return subNodes[1].execute(instance, script);
+		} else {
+			if (subNodes[2] != null) {
+				return subNodes[2].execute(instance, script);
+			}
+		}
+
+		return new Variable(1);
+	}
+
+	@Override
+	public void print(String indentation) {
+		System.out.println(indentation + "If Statement");
+		subNodes[0].print(indentation + "-");
+		subNodes[1].print(indentation + ".");
+		if (subNodes[2] != null) {
+			subNodes[2].print(indentation + ".");
+		}
+	}
+}
